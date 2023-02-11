81d18cdda6bd4b5c9ccf02e668f4ba3f61e00e51
김택윤 <gw9122@naver.com>
2023-02-11 18:49:47 +0900

    HelloWorld proj init

---
+++ hello_word/android/app/src/main/kotlin/site/taxijjang/hello_word/MainActivity.kt
@@ -0,0 +1,6 @@
+package site.taxijjang.hello_word
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ hello_word/lib/main.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MaterialApp(
+      home: Scaffold(
+    backgroundColor: Colors.black,
+    body: Center(
+      child: Text('Hello World',
+          style: TextStyle(
+            color: Colors.white,
+            fontSize: 30,
+          )),
+    ),
+  )));
+}
