800d3b4bb85669bdc50182d0a5a947efc42ae9ec
김택윤 <gw9122@naver.com>
2023-02-19 10:14:33 +0900

    binding 추가

--- web_view/lib/main.dart
+++ web_view/lib/main.dart
@@ -4,7 +4,7 @@ import 'package:web_view/screen/home_screen.dart';
 void main() {
   // Flutter 프레임워크가
   // 웹을 실행할 준비가 될때까지 기다린다.
-  // WidgetsFlutterBinding.ensureInitialized();
+  WidgetsFlutterBinding.ensureInitialized();
 
   runApp(
     MaterialApp(
